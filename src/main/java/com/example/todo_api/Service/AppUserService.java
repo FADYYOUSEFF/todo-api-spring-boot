f8027d7f65528b9404cb70bd0f1c0dd96dd1b52a
fady <fadyyoussef4444@gamil.com>
2025-07-20 15:03:07 +0300

    Implement AppUserService and TodoService for user management and todo operations

---
+++ src/main/java/com/example/todo_api/Service/AppUserService.java
@@ -0,0 +1,55 @@
+package com.example.todo_api.Service;
+
+import com.example.todo_api.Config.PasswordConfig;
+import com.example.todo_api.Dto.UserDto;
+import com.example.todo_api.Dto.UserMapper;
+import com.example.todo_api.Entitiy.UserApp;
+import com.example.todo_api.Entitiy.UserPrinciple;
+import com.example.todo_api.Exception.ExceptionMessage;
+import com.example.todo_api.Exception.RecordDublicatedException;
+import com.example.todo_api.Exception.RecordNotFoundException;
+import com.example.todo_api.Exception.UserNotFoundException;
+import com.example.todo_api.Repository.UserAppRpo;
+import com.example.todo_api.payload.UserRequest;
+import com.example.todo_api.payload.UserRequestMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class AppUserService implements UserDetailsService {
+    @Autowired
+    UserAppRpo userAppRpo;
+    @Autowired
+    PasswordConfig passwordConfig;
+    @Autowired
+    UserMapper userMapper;
+    @Autowired
+    UserRequestMapper userRequestMapper;
+
+    public UserDto saveUser(UserRequest userRequest) {
+        UserApp userApp = userRequestMapper.map(userRequest);
+        boolean isUserNameExist=userAppRpo.findByUsername(userApp.getusername()).isPresent();
+        if(isUserNameExist){
+            throw new RecordDublicatedException(ExceptionMessage.USERNAME_TAKEN);
+        }
+        userApp.setPassword(passwordConfig.passwordEncoder().encode(userApp.getPassword()));
+        UserApp userAdded=userAppRpo.save(userApp);
+        return userMapper.map(userAdded);
+    }
+
+    public UserApp findByUsername(String username){
+        return userAppRpo.findByUsername(username)
+                .orElseThrow(()->new UserNotFoundException(ExceptionMessage.USER_NOT_FOUND));
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserApp user = userAppRpo.findByUsername(username)
+                .orElseThrow(() -> new UserNotFoundException(ExceptionMessage.USER_NOT_FOUND));
+        return new UserPrinciple(user);
+    }
+}
---
+++ src/main/java/com/example/todo_api/Service/TodoService.java
@@ -0,0 +1,78 @@
+package com.example.todo_api.Service;
+
+import com.example.todo_api.Dto.TodoDto;
+import com.example.todo_api.Dto.TodoMapper;
+import com.example.todo_api.Entitiy.ToDo;
+import com.example.todo_api.Entitiy.UserApp;
+import com.example.todo_api.Exception.ExceptionMessage;
+import com.example.todo_api.Exception.RecordNotFoundException;
+import com.example.todo_api.Repository.TodoRepo;
+import com.example.todo_api.payload.TodoRequest;
+import com.example.todo_api.payload.TodoRequestMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+    @Autowired
+    TodoRepo todoRepo;
+    @Autowired
+    TodoMapper todoMapper;
+    @Autowired
+    AppUserService appUserService;
+    @Autowired
+    TodoRequestMapper todoRequestMapper;
+
+    public TodoDto addTodo(TodoRequest todoRequest) {
+        ToDo todo=todoRequestMapper.map(todoRequest);
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        UserApp userApp = appUserService.findByUsername(username);
+        ToDo newTodo = new ToDo();
+        newTodo.setUser(userApp);
+        newTodo.setTitle(todo.getTitle());
+        newTodo.setCompleted(todo.isCompleted());
+        newTodo.setDescription(todo.getDescription());
+        todoRepo.save(newTodo);
+        return todoMapper.map(newTodo);
+    }
+
+
+    public ToDo getTodoById(long id) {
+        return todoRepo.findById(id).orElseThrow(() -> new RecordNotFoundException(ExceptionMessage.RECORD_NOT_FOUND));
+    }
+
+    public TodoDto updateTodo(Long id,TodoRequest todoRequest) {
+        ToDo todo=todoRequestMapper.map(todoRequest);
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        ToDo todoToBeUpdated = this.getTodoById(id);
+        // throw record not found exception in case the todos it is not exist or the todos stored in the database not for the given user
+        if (todoToBeUpdated == null || !todoToBeUpdated.getUser().getusername().equals(username)) {
+            throw new RecordNotFoundException(ExceptionMessage.RECORD_NOT_FOUND);
+        }
+        todoToBeUpdated.setTitle(todo.getTitle());
+        todoToBeUpdated.setDescription(todo.getDescription());
+        todoToBeUpdated.setCompleted(todo.isCompleted());
+        todoRepo.save(todoToBeUpdated);
+        return todoMapper.map(todoToBeUpdated);
+    }
+
+    public TodoDto deleteTodo(long id) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        ToDo todo = this.getTodoById(id);
+        if (todo == null || !todo.getUser().getusername().equals(username)) {
+            throw new RecordNotFoundException(ExceptionMessage.RECORD_NOT_FOUND);
+        }
+        todoRepo.deleteById(id);
+        return todoMapper.map(todo);
+    }
+
+    public List<TodoDto> getTodos() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        UserApp userApp = appUserService.findByUsername(username);
+        List<ToDo> toDos = todoRepo.findTodoByUserId(userApp.getId());
+        return todoMapper.map(toDos);
+    }
+}
