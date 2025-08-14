4874d2dbb9c61f121fe024e1bf704489e7a80532
fady <fadyyoussef4444@gamil.com>
2025-08-14 17:57:49 +0300

    Fix typo in ExceptionMessage class for RECORD_DUPLICATED constant

--- src/main/java/com/example/todo_api/Exception/ExceptionMessage.java
+++ src/main/java/com/example/todo_api/Exception/ExceptionMessage.java
@@ -2,7 +2,8 @@ package com.example.todo_api.Exception;
 
 public class ExceptionMessage {
     public final static String RECORD_NOT_FOUND ="Record not found.";
-    public final static String RECORD_DUBLICATED ="Record is already exist.";
+    public final static String RECORD_DUPLICATED ="Record is already exist.";
     public final static String USERNAME_TAKEN="Username is already taken";
     public final static String USER_NOT_FOUND="User not found";
+    public final static String BAD_REQUEST="bad request";
 }
